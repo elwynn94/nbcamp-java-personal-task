a018a08727abf38c67bd1265694c175138a826e4
Robbie <robbie331@>
2024-04-29 13:56:18 +0900

    Level1 - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -13,8 +13,20 @@ public class App {
         System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, / |
         char c = sc.next().charAt(0); // 사용자로부터 입력받은 문자열의 첫 번째(0) 문자를 c라는 변수에 저장.
 
-
-
-
+        if (c == '+') {
+            System.out.println("결과 : " + (a + b));
+        } else if (c == '-') {
+            System.out.println("결과 : " + (a - b));
+        } else if (c == '*') {
+            System.out.println("결과 : " + (a * b));
+        } else if (c == '/') {
+            if (b != 0) {
+                System.out.println("결과 : " + (a / b));
+            } else {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            }
+        } else {
+            System.out.println("사칙연산 기호만 넣어주세요. (+, -, *, /)");
         }
     }
+}
