94a44db74ecb3ec15ca571b223a710ee37abd00e
Robbie <robbie331@>
2024-04-29 12:57:00 +0900

    Level1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int a = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int b = sc.nextInt();
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int a = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int b = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, / |
+        char c = sc.next().charAt(0); // 사용자로부터 입력받은 문자열의 첫 번째(0) 문자를 c라는 변수에 저장.
 
-            System.out.println(a + b);
+
+
+
         }
     }
