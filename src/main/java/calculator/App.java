fad92b92460dea40a59abcaedd001f2ef2141e94
Robbie <robbie331@>
2024-05-02 20:29:12 +0900

    Level2 - 4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,14 +1,12 @@
 package calculator;
 
-import java.util.LinkedList;
 import java.util.Objects;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Queue<Integer> results = new LinkedList<>(); // results Queue 생성
+        Calculator calculator = new Calculator();
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -18,45 +16,30 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, / |
             char c = sc.next().charAt(0); // 사용자로부터 입력받은 문자열의 첫 번째(0) 문자를 c라는 변수에 저장합니다.
 
-            int result = 0; // 연산 결과를 저장할 변수 result 생성
+            int results = 0;
 
-            if (c == '+' || c == '-' || c == '*' || c == '/') {
-                if (c == '+') {
-                    result = a + b;
-                } else if (c == '-') {
-                    result = a - b;
-                } else if (c == '*') {
-                    result = a * b;
-                } else if (c == '/') {
-                    if (b == 0) {
-                        System.out.println("나눗셈 연산에서 분모가 0이 입력될 수 없습니다.");
-                    } else {
-                        result = a / b;
-                    }
-                }
+            try {
+                results = calculator.calculate(a, b, c);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                // 예외 후 처리
+            }
 
-                System.out.println("결과 : " + result);
-                results.add(result); // 결과값을 results Queue에 추가
+            calculator.getResults().add(results);
 
-
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                String remove = sc.next();
-                if (remove.equals("remove")) {
-                    results.remove(results.poll()); // 가장 최신 결과값을 꺼냄 (삭제)
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String remove = sc.next();
+            if (remove.equals("remove")) {
+                    calculator.removeResult(0); // 가장 최신 결과값을 꺼냄 (삭제)
                 }
 
                 System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
                 String inquiry = sc.next();
                 if (inquiry.equals("inquiry")) {
-                    for (Integer i : results) {
+                    for (Integer i : calculator.getResults()) {
                         System.out.println(i);
                     }
                 }
-
-            } else {
-                System.out.println("사칙연산 기호만 넣어주세요. (+, -, *, /)");
-                continue;
-            }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String moreCalculate = sc.next();
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,6 @@
 package calculator;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 // 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후 결과 값을 반환하는
 // 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.
@@ -9,48 +8,41 @@ import java.util.Queue;
 //    (매개변수로 해당 오류 내용을 전달합니다.)
 
 public class Calculator {
-    private Queue<Integer> results;
+    private final Queue<Integer> results = new LinkedList<>();
 
-    public Calculator() {
-        this.results = new LinkedList<>();
-    }
 
-    public int calculate(int a, int b, char c) throws InvalidOperatorException, DivisionByZeroException {
+
+    public int calculate(int a, int b, char c) {
         int result = 0; // 연산 결과를 저장할 변수 result 생성
 
-        if (c == '+' || c == '-' || c == '*' || c == '/') {
-            if (c == '+') {
-                result = a + b;
-            } else if (c == '-') {
-                result = a - b;
-            } else if (c == '*') {
-                result = a * b;
-            } else if (c == '/') {
-                if (b == 0) {
-                    throw new DivisionByZeroException("나눗셈 연산에서 분모가 0이 입력되었습니다.");
-                } else {
-                    result = a / b;
-                }
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
+                if (c == '+') {
+                    result = a + b;
+                } else if (c == '-') {
+                    result = a - b;
+                } else if (c == '*') {
+                    result = a * b;
+                } else if (c == '/') {
+                    if (b == 0) {
+                        throw new ArithmeticException("나눗셈 연산에서 분모가 0이 입력될 수 없습니다.");
+                    } else {
+                        result = a / b;
+                    }
 
-                System.out.println("결과 : " + result);
-                results.add(result); // 결과값을 results Queue에 추가
+                    System.out.println("결과 : " + result);
+                    results.add(result); // 결과값을 results Queue에 추가
+                }
+            } else {
+                throw new UnsupportedOperationException("사칙연산 기호만 넣어주세요. (+, -, *, /)");
             }
-        } else {
-            throw new InvalidOperatorException("사칙연산 기호만 넣어주세요. (+, -, *, /)");
-        }
         return result;
     }
 
-    // 예외 클래스 정의
-    static class DivisionByZeroException extends Exception {
-        public DivisionByZeroException(String message) {
-            super(message);
-        }
+    public Queue<Integer> getResults() {
+        return results;
     }
 
-    static class InvalidOperatorException extends Exception {
-        public InvalidOperatorException(String message) {
-            super(message);
-        }
+    public void removeResult(int index) { // 삭제 Method
+        this.results.remove(index);
     }
 }
